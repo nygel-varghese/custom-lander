c348c556e24583e4d1c77cb92515648bc4539b71
nygel-varghese <nygelvarghese@gmail.com>
2026-09-21 12:26:16 +0400

    Updated Script for October deadline
    Updated the deadline a second time to 5th of October

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@
 */
 
 const CONFIG = {
-  launchDate: "2026-09-19T12:00:00+05:30",
+  launchDate: "2026-10-05T12:00:00+05:30",
 
   quotes: [
   "Curiosity is a better starting point than certainty.",
